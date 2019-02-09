50968213efe1ce503d15bec360af7c4ea34b7bfb
Mike Dowd <mikedowd1972@gmail.com>
2019-02-09 10:21:25 -0700

    Initial commit

---
+++ src/components/my-test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './my-test.scss';
+
+class MyTest extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {};
+  }
+
+  render = () => <div className="master-wrapper">HELLO WORLD</div>
+}
+
+export default MyTest;
